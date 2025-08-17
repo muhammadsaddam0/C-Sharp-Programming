bd4634ec16ef9fcd1573cce726885f74fa90816e
muhammadsaddam0 <saddamiqbal20@gmail.com>
2025-08-17 19:19:11 +0500

    Add Practice 4 Functions

---
+++ Practice 4 Functions/Functions/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Functions
+{
+
+    class Test
+    {
+         public  void something()
+        {
+            Console.WriteLine("class test");
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class program");
+            WelcomeMessage();
+
+            Test test = new Test(); //<--"instance" used when other class that you want to call function from, don't have used (static)
+            test.something();
+
+            //Test.something(); <-- (calling funtion) used when "Class Test" have "public static" used
+          
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("helloP");
+        }
+    }
+}
