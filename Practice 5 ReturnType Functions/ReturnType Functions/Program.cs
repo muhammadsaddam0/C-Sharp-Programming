5be2b159ef445e3600b415140b79b0f3216ce0dd
muhammadsaddam0 <saddamiqbal20@gmail.com>
2025-08-19 03:02:00 +0500

    Add Practice 5 ReturnType Functions

---
+++ Practice 5 ReturnType Functions/ReturnType Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+
+namespace ReturnType_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = ReadNumberFromConsole();
+            }
+
+            foreach (int item in numbers)
+            {
+                Console.Write($" {item} ");
+            }
+            
+        }
+        static int ReadNumberFromConsole()
+        {
+            Console.Write("Enter no : ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+    }
+}
