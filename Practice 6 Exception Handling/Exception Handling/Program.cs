0b2a5c5346cacbc00f1601b608cff81646ea69f8
muhammadsaddam0 <saddamiqbal20@gmail.com>
2025-08-24 05:22:32 +0500

    Add Practice 6 Exception Handling

---
+++ Practice 6 Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // exception = errors that occur during execution
+
+            //        try     = try some code that is considered "dangerous"
+            //        catch   = catches and handles exceptions when they occur
+            //        finally = always executes regardless if exception is caught or not
+
+            int x;
+            int y;
+            double result;
+
+            try
+            {
+                Console.Write("Enter number 1: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                result = (double)x / y;
+
+                Console.WriteLine("result: " + result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter ONLY numbers PLEASE!");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You can't divide by zero! IDIOT!");
+            }
+            catch (Exception e)// handles all exception but we can't tell the user what actually is wrong (not good aproach for programmer) 
+            { 
+                Console.WriteLine("Something went wrong!");
+            }
+            finally// executes when no exception occurs
+            {
+                Console.WriteLine("Thanks for visiting!");
+            }
+
+
+        }
+    }
+}
